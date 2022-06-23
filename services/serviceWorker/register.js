9fdf37e28dcb62ca7a5e624d57d5b61b1f4b9c29
George Apraksin <dreamvibe@mail.ru>
2022-06-24 01:08:18 +0500

    trying to fix no service worker

--- services/serviceWorker/register.js
+++ services/serviceWorker/register.js
@@ -1,23 +1,21 @@
 export const registerServiceWorker = () => {
   if (typeof window !== "undefined") {
-    window.addEventListener("load", () => {
-      if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("/sw.js").then(
-          function (registration) {
-            console.log(
-              "Service Worker registration successful with scope: ",
-              registration.scope
-            );
-            navigator.serviceWorker.onmessage = function (e) {
-              // messages from service worker.
-              console.log("e.data", e.data);
-            };
-          },
-          function (err) {
-            console.log("Service Worker registration failed: ", err);
-          }
-        );
-      }
-    });
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("/sw.js").then(
+        function (registration) {
+          console.log(
+            "Service Worker registration successful with scope: ",
+            registration.scope
+          );
+          navigator.serviceWorker.onmessage = function (e) {
+            // messages from service worker.
+            console.log("e.data", e.data);
+          };
+        },
+        function (err) {
+          console.log("Service Worker registration failed: ", err);
+        }
+      );
+    }
   }
 };
